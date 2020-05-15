e5abd8e1d17e40acb09bd7443b8533f00e7ed230
srajiang <srajiang@gmail.com>
2020-05-15 10:57:07 -0700

    Add post-commit hook

--- test/scripts/cucumber-runner.js
+++ test/scripts/cucumber-runner.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-console.log('CUCUMBER RUNNER ACTIVE')
 var os = require("os");
 var child_process = require("child_process");
 var config_file =
@@ -7,35 +6,20 @@ var config_file =
 var config = require(config_file).config;
 var command = "/usr/bin/env";
 
-// console.log('argv0 before', process.argv[0]);
-// console.log('argv1 before', process.argv[1]);
-
-// process.argv[0] = "node";
 process.argv[1] = "./node_modules/cucumber/bin/cucumber.js";
-// process.argv[1] = "/Users/sarahjiang/.jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/NodeJS/lib/node_modules/cucumber/bin/cucumber.js";
-
-// console.log("argv0 after", process.argv[0]);
-// console.log("argv1 after", process.argv[1]);
 
 // Check if os is windows
 if (os.platform() == "win32") {
   command = process.argv.shift();
 }
 
-for (var i in config.capabilities) {
-  
-  
-  console.log("PROCESS", config.capabilities[i].browserName);
-
-  // console.log('process env', process.env);
+for (var i in config.capabilities) {  
   var env = Object.create(process.env);
-  // console.log('env', env);
-
   env.TASK_ID = i.toString();
 
   var p = child_process.spawn(command, process.argv, { env: env });
 
-  p.on('exit', (code, signal) => console.log('code', code, 'signal', signal));
+  p.on('exit', (code, signal) => console.log('exited with code:', code, 'and signal:', signal));
   p.on('error', () => console.log('there was an error with childprocess'));
   p.stdout.on("data", (data) => {
     console.log(`childprocess stdout:\n${data}`);
