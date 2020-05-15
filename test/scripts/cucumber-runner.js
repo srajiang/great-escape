e9f10a2b23684d191f89e9bfe9708cd8e4013f29
srajiang <srajiang@gmail.com>
2020-05-14 22:12:34 -0700

    Add console logging for debugging output

--- test/scripts/cucumber-runner.js
+++ test/scripts/cucumber-runner.js
@@ -7,14 +7,16 @@ var config_file =
 var config = require(config_file).config;
 var command = "/usr/bin/env";
 
-
-console.log('os', os);
-console.log('config file path', config_file)
+console.log('argv0 before', process.argv[0]);
+console.log('argv1 before', process.argv[1]);
 
 process.argv[0] = "node";
 // process.argv[1] = "./node_modules/cucumber/bin/cucumber.js";
-process.argv[1] = "node_modules/cucumber/bin/cucumber.js";
+process.argv[1] = "./lib/node_modules/cucumber/bin/cucumber.js";
 // process.argv[1] = "/Users/sarahjiang/.jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/NodeJS/lib/node_modules/cucumber/bin/cucumber.js";
+
+console.log("argv0 after", process.argv[0]);
+console.log("argv1 after", process.argv[1]);
 
 // Check if os is windows
 if (os.platform() == "win32") {
@@ -24,7 +26,12 @@ if (os.platform() == "win32") {
 for (var i in config.capabilities) {
   console.log("PROCESS", config.capabilities[i].browserName);
   var env = Object.create(process.env);
+
+  console.log(env);
+
   env.TASK_ID = i.toString();
+
   var p = child_process.spawn(command, process.argv, { env: env });
+
   p.stdout.pipe(process.stdout);
 }
